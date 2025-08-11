a37eeffb632a5305c314408674d1ea0d800893d8
Test-Net009 <nikhil.shelke@quickheal.com>
2025-08-11 17:50:13 +0530

    Latest Cookie consent
    test

--- seqrite-config.js
+++ seqrite-config.js
@@ -1,14 +1,14 @@
 
 let userLang = (navigator.language || (navigator).userLanguage || 'en').slice(0, 2);
 window.klaroConfig = {
-  "consentManagerId": "686d14e254516a2464531419",
-  "uuid": "7e508dfa-da09-4941-b9a1-df7c31e38857",
+  "consentManagerId": "68996bb454516a2464531427",
+  "uuid": "95bd03e0-eb3e-4a05-9a43-be9784cfe31a",
   "domain": "https://qa-hs.qhtpl.com/cmapi",
   "version": 1,
   "elementID": "SEQRITE",
   lang: userLang,
   "storageMethod": "cookie",
-  "storageName": "https://lucky-empanada-c5a13c.netlify.app/",
+  "storageName": "https://cookieconsenttestqa.ncetlify.app/",
   "cookieExpiresAfterDays": 365,
   "default": false,
   "mustConsent": false,
@@ -41,7 +41,7 @@ window.klaroConfig = {
       "poweredBy": "Powered by Seqrite",
       "purposes": {
         "Analytics": {
-          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance. </p>"
+          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance.</p>"
         },
         "Others": {
           "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p>"
@@ -87,7 +87,7 @@ window.klaroConfig = {
       "poweredBy": "Powered by Seqrite",
       "purposes": {
         "Analytics": {
-          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance. </p>"
+          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance.</p>"
         },
         "Others": {
           "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p>"
@@ -133,7 +133,7 @@ window.klaroConfig = {
       "poweredBy": "Powered by Seqrite",
       "purposes": {
         "Analytics": {
-          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance. </p>"
+          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance.</p>"
         },
         "Others": {
           "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p>"
@@ -162,53 +162,7 @@ window.klaroConfig = {
       "acceptSelected": "निवडलेले स्वीकारा",
       "ok": "सर्व स्वीकारा",
       "consentNotice": {
-        "description": "<p><strong>आम्ही तुमच्या गोपनीयतेचा आदर करतो.</strong></p><p>आम्ही साइटची आवश्यक कार्यक्षमता तसेच मार्केटिंग, वैयक्तिकरण आणि विश्लेषण सक्षम करण्यासाठी कुकीज संग्रहित करतो. \"सर्व स्वीकारा\" वर क्लिक करून, तुम्ही आमच्या कुकीजच्या वापरास संमती देता. तुम्ही कधीही तुमच्या सेटिंग्ज बदलू शकता. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>कुकी धोरण.</u></a></p>",
-        "learnMore": "सानुकूलित करा",
-        "changeDescription": " "
-      },
-      "save": "स्वीकारा",
-      "consentModal": {
-        "title": "<p>संमती प्राधान्ये कस्टमाइझ करा</p>",
-        "description": "<p>आम्ही कुकीजचा वापर तुम्हाला पृष्ठांमध्ये कार्यक्षमतेने नेव्हिगेट करण्यास, तुमच्या पसंती संग्रहित करण्यास आणि सामान्यतः वेबसाइटवरील तुमचा अनुभव सुधारण्यास मदत करण्यासाठी करतो. तथापि, आम्ही तुमचा गोपनीयतेचा अधिकार मान्य करतो आणि म्हणून तुम्ही काही प्रकारच्या कुकीजना परवानगी न देणे निवडू शकता. अधिक जाणून घेण्यासाठी आणि आमच्या डीफॉल्ट सेटिंग्ज बदलण्यासाठी वेगवेगळ्या श्रेणी शीर्षकांवर क्लिक करा. कृपया लक्षात ठेवा की विशिष्ट प्रकारच्या कुकीज अवरोधित केल्याने साइटवरील तुमच्या अनुभवावर परिणाम होऊ शकतो.</p>",
-        "buttons": {
-          "save": "स्वीकारा",
-          "acceptAll": "सर्व स्वीकारा",
-          "decline": "सर्व नाकारा"
-        }
-      },
-      "poweredBy": "Powered by Seqrite",
-      "purposes": {
-        "Analytics": {
-          "description": "<p>या कुकीज आम्हाला वेबसाइट ट्रॅफिक, वापरकर्त्याचे वर्तन आणि प्रतिबद्धता नमुन्यांचे विश्लेषण करण्यास मदत करतात. गोळा केलेल्या अंतर्दृष्टी आम्हाला सामग्री ऑप्टिमाइझ करण्यास, वापरण्यायोग्यता वाढविण्यास आणि एकूण साइट कार्यप्रदर्शन सुधारण्यास अनुमती देतात.</p>"
-        },
-        "Others": {
-          "description": "<p>या श्रेणीमध्ये अशा कुकीज समाविष्ट आहेत ज्या पूर्वनिर्धारित श्रेणींमध्ये बसत नाहीत परंतु काही वेबसाइट कार्यक्षमता किंवा सुधारणांसाठी आवश्यक असू शकतात.</p>"
-        }
-      },
-      "service": {
-        "disableAll": {
-          "description": "DisableAll",
-          "title": "Disable"
-        },
-        "optOut": {
-          "description": "Opt Out",
-          "title": "Opt Out"
-        },
-        "purpose": "उद्देश"
-      },
-      "purposeItem": {
-        "service": "सेवा",
-        "services": "सेवा"
-      }
-    },
-    "mr": {
-      "acceptAll": "सर्व स्वीकारा",
-      "accept": "स्वीकारा",
-      "decline": "सर्व नाकारा",
-      "acceptSelected": "निवडलेले स्वीकारा",
-      "ok": "सर्व स्वीकारा",
-      "consentNotice": {
-        "description": "<p><strong>आम्ही तुमच्या गोपनीयतेचा आदर करतो.</strong></p><p> आम्ही साइटची आवश्यक कार्यक्षमता तसेच मार्केटिंग, वैयक्तिकरण आणि विश्लेषण सक्षम करण्यासाठी कुकीज संग्रहित करतो. &quot;सर्व स्वीकारा&quot; वर क्लिक करून, तुम्ही आमच्या कुकीजच्या वापरास संमती देता. तुम्ही कधीही तुमच्या सेटिंग्ज बदलू शकता. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>कुकी धोरण.</u></a></p>",
+        "description": "<p><strong>आम्ही तुमच्या गोपनीयतेचा आदर करतो.</strong></p><p> आम्ही आवश्यक साइट कार्यक्षमता तसेच मार्केटिंग, वैयक्तिकरण आणि विश्लेषण सक्षम करण्यासाठी कुकीज संग्रहित करतो. &quot;सर्व स्वीकारा&quot; वर क्लिक करून, तुम्ही आमच्या कुकीजच्या वापरास संमती देता. तुम्ही कधीही तुमच्या सेटिंग्ज बदलू शकता. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>कुकी धोरण.</u></a></p>",
         "learnMore": "सानुकूलित करा",
         "changeDescription": " "
       },
@@ -247,34 +201,34 @@ window.klaroConfig = {
         "services": "सेवा"
       }
     },
-    "bengali": {
-      "acceptAll": "সব গ্রহণ করুন",
-      "accept": "গ্রহণ করুন",
-      "decline": "সব প্রত্যাখ্যান করুন",
-      "acceptSelected": "নির্বাচিত গ্রহণ করুন",
-      "ok": "সব গ্রহণ করুন",
+    "mr": {
+      "acceptAll": "सर्व स्वीकारा",
+      "accept": "स्वीकारा",
+      "decline": "सर्व नाकारा",
+      "acceptSelected": "निवडलेले स्वीकारा",
+      "ok": "सर्व स्वीकारा",
       "consentNotice": {
-        "description": "<p><strong>আমরা আপনার গোপনীয়তাকে সম্মান করি</strong></p><p> আমরা সাইটের প্রয়োজনীয় কার্যকারিতা, সেইসাথে মার্কেটিং, ব্যক্তিগতকরণ এবং বিশ্লেষণ সক্ষম করার জন্য কুকিজ সংরক্ষণ করি। &quot;সব গ্রহণ করুন&quot; এ ক্লিক করে, আপনি আমাদের কুকিজ ব্যবহারে সম্মতি দিচ্ছেন। আপনি যেকোনো সময় আপনার সেটিংস পরিবর্তন করতে পারেন। <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>কুকি নীতি।</u></a></p>",
-        "learnMore": "কাস্টমাইজ করুন",
+        "description": "<p><strong>आम्ही तुमच्या गोपनीयतेचा आदर करतो.</strong></p><p> आम्ही आवश्यक साइट कार्यक्षमता तसेच मार्केटिंग, वैयक्तिकरण आणि विश्लेषण सक्षम करण्यासाठी कुकीज संग्रहित करतो. &quot;सर्व स्वीकारा&quot; वर क्लिक करून, तुम्ही आमच्या कुकीजच्या वापरास संमती देता. तुम्ही कधीही तुमच्या सेटिंग्ज बदलू शकता. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>कुकी धोरण.</u></a></p>",
+        "learnMore": "सानुकूलित करा",
         "changeDescription": " "
       },
-      "save": "গ্রহণ করুন",
+      "save": "स्वीकारा",
       "consentModal": {
-        "title": "সম্মতি পছন্দগুলি কাস্টমাইজ করুন",
-        "description": "আমরা কুকিজ ব্যবহার করি আপনাকে দক্ষতার সাথে বিভিন্ন পৃষ্ঠায় নেভিগেট করতে, আপনার পছন্দগুলি সংরক্ষণ করতে এবং সাধারণত ওয়েবসাইটের অভিজ্ঞতা উন্নত করতে। তবে, আমরা আপনার গোপনীয়তার অধিকার স্বীকার করি এবং তাই আপনি কিছু ধরণের কুকিজকে অনুমতি না দেওয়ার সিদ্ধান্ত নিতে পারেন। আরও জানতে এবং আমাদের ডিফল্ট সেটিংস পরিবর্তন করতে বিভিন্ন বিভাগের শিরোনামে ক্লিক করুন। দয়া করে মনে রাখবেন যে নির্দিষ্ট ধরণের কুকিজ ব্লক করা সাইটের আপনার অভিজ্ঞতাকে প্রভাবিত করতে পারে। ",
+        "title": "संमती प्राधान्ये कस्टमाइझ करा",
+        "description": "आम्ही कुकीजचा वापर तुम्हाला पृष्ठांमध्ये कार्यक्षमतेने नेव्हिगेट करण्यास, तुमच्या पसंती संग्रहित करण्यास आणि सामान्यतः वेबसाइटवरील तुमचा अनुभव सुधारण्यास मदत करण्यासाठी करतो. तथापि, आम्ही तुमचा गोपनीयतेचा अधिकार मान्य करतो आणि म्हणून तुम्ही काही प्रकारच्या कुकीजना परवानगी न देणे निवडू शकता. अधिक जाणून घेण्यासाठी आणि आमच्या डीफॉल्ट सेटिंग्ज बदलण्यासाठी वेगवेगळ्या श्रेणी शीर्षकांवर क्लिक करा. कृपया लक्षात ठेवा की विशिष्ट प्रकारच्या कुकीज अवरोधित केल्याने साइटवरील तुमच्या अनुभवावर परिणाम होऊ शकतो. ",
         "buttons": {
-          "save": "গ্রহণ করুন",
-          "acceptAll": "সব গ্রহণ করুন",
-          "decline": "সব প্রত্যাখ্যান করুন"
+          "save": "स्वीकारा",
+          "acceptAll": "सर्व स्वीकारा",
+          "decline": "सर्व नाकारा"
         }
       },
       "poweredBy": "Powered by Seqrite",
       "purposes": {
         "Analytics": {
-          "description": "<p>এই কুকিজ আমাদের ওয়েবসাইট ট্র্যাফিক, ব্যবহারকারীর আচরণ এবং ব্যস্ততার ধরণ বিশ্লেষণ করতে সাহায্য করে। সংগৃহীত অন্তর্দৃষ্টি আমাদের কন্টেন্ট অপ্টিমাইজ করতে, ব্যবহারযোগ্যতা বাড়াতে এবং সামগ্রিক সাইটের কর্মক্ষমতা উন্নত করতে সাহায্য করে।</p>"
+          "description": "<p>या कुकीज आम्हाला वेबसाइट ट्रॅफिक, वापरकर्त्याचे वर्तन आणि प्रतिबद्धता नमुन्यांचे विश्लेषण करण्यास मदत करतात. गोळा केलेल्या अंतर्दृष्टी आम्हाला सामग्री ऑप्टिमाइझ करण्यास, वापरण्यायोग्यता वाढविण्यास आणि एकूण साइट कार्यप्रदर्शन सुधारण्यास अनुमती देतात.</p>"
         },
         "Others": {
-          "description": "<p>এই বিভাগে এমন কুকিজ অন্তর্ভুক্ত রয়েছে যা পূর্বনির্ধারিত বিভাগগুলিতে খাপ খায় না তবে নির্দিষ্ট ওয়েবসাইটের কার্যকারিতা বা উন্নতির জন্য প্রয়োজনীয় হতে পারে।</p>"
+          "description": "<p>या श्रेणीमध्ये अशा कुकीज समाविष्ट आहेत ज्या पूर्वनिर्धारित श्रेणींमध्ये बसत नाहीत परंतु काही वेबसाइट कार्यक्षमता किंवा सुधारणांसाठी आवश्यक असू शकतात.</p>"
         }
       },
       "service": {
@@ -286,41 +240,41 @@ window.klaroConfig = {
           "description": "Opt Out",
           "title": "Opt Out"
         },
-        "purpose": "উদ্দেশ্য"
+        "purpose": "उद्देश"
       },
       "purposeItem": {
-        "service": "সেবা",
-        "services": "সেবা"
+        "service": "सेवा",
+        "services": "सेवा"
       }
     },
-    "bn": {
-      "acceptAll": "সব গ্রহণ করুন",
-      "accept": "গ্রহণ করুন",
-      "decline": "সব প্রত্যাখ্যান করুন",
-      "acceptSelected": "নির্বাচিত গ্রহণ করুন",
-      "ok": "সব গ্রহণ করুন",
+    "hindi": {
+      "acceptAll": "सभी स्वीकृत",
+      "accept": "स्वीकार करना",
+      "decline": "सभी अस्वीकार करें",
+      "acceptSelected": "चयनित स्वीकार करें",
+      "ok": "सभी स्वीकृत",
       "consentNotice": {
-        "description": "<p><strong>আমরা আপনার গোপনীয়তাকে সম্মান করি</strong></p><p> আমরা সাইটের প্রয়োজনীয় কার্যকারিতা, সেইসাথে মার্কেটিং, ব্যক্তিগতকরণ এবং বিশ্লেষণ সক্ষম করার জন্য কুকিজ সংরক্ষণ করি। &quot;সব গ্রহণ করুন&quot; এ ক্লিক করে, আপনি আমাদের কুকিজ ব্যবহারে সম্মতি দিচ্ছেন। আপনি যেকোনো সময় আপনার সেটিংস পরিবর্তন করতে পারেন। <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>কুকি নীতি।</u></a></p>",
-        "learnMore": "কাস্টমাইজ করুন",
+        "description": "<p><strong>हम आपकी गोपनीयता का सम्मान करते हैं</strong></p><p>हम आवश्यक साइट कार्यक्षमता, साथ ही मार्केटिंग, वैयक्तिकरण और विश्लेषण को सक्षम करने के लिए कुकीज़ संग्रहीत करते हैं। \"सभी स्वीकार करें\" पर क्लिक करके, आप कुकीज़ के हमारे उपयोग के लिए सहमति देते हैं। आप अपनी सेटिंग कभी भी बदल सकते हैं। <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>कुकी नीति।</u></a></p>",
+        "learnMore": "अनुकूलित करें",
         "changeDescription": " "
       },
-      "save": "গ্রহণ করুন",
+      "save": "स्वीकार करना",
       "consentModal": {
-        "title": "সম্মতি পছন্দগুলি কাস্টমাইজ করুন",
-        "description": "আমরা কুকিজ ব্যবহার করি আপনাকে দক্ষতার সাথে বিভিন্ন পৃষ্ঠায় নেভিগেট করতে, আপনার পছন্দগুলি সংরক্ষণ করতে এবং সাধারণত ওয়েবসাইটের অভিজ্ঞতা উন্নত করতে। তবে, আমরা আপনার গোপনীয়তার অধিকার স্বীকার করি এবং তাই আপনি কিছু ধরণের কুকিজকে অনুমতি না দেওয়ার সিদ্ধান্ত নিতে পারেন। আরও জানতে এবং আমাদের ডিফল্ট সেটিংস পরিবর্তন করতে বিভিন্ন বিভাগের শিরোনামে ক্লিক করুন। দয়া করে মনে রাখবেন যে নির্দিষ্ট ধরণের কুকিজ ব্লক করা সাইটের আপনার অভিজ্ঞতাকে প্রভাবিত করতে পারে। ",
+        "title": "<p>सहमति प्राथमिकताएँ अनुकूलित करें</p>",
+        "description": "<p>हम कुकीज़ का इस्तेमाल आपको पृष्ठों के बीच कुशलतापूर्वक नेविगेट करने, आपकी प्राथमिकताओं को संग्रहीत करने और वेबसाइट के आपके अनुभव को बेहतर बनाने में मदद करने के लिए करते हैं। हालाँकि, हम आपकी गोपनीयता के अधिकार को स्वीकार करते हैं और इसलिए आप कुछ प्रकार की कुकीज़ की अनुमति नहीं देने का विकल्प चुन सकते हैं। अधिक जानकारी प्राप्त करने और हमारी डिफ़ॉल्ट सेटिंग्स बदलने के लिए विभिन्न श्रेणी शीर्षकों पर क्लिक करें। कृपया ध्यान दें कि कुछ प्रकार की कुकीज़ को ब्लॉक करने से साइट के आपके अनुभव पर असर पड़ सकता है।</p>",
         "buttons": {
-          "save": "গ্রহণ করুন",
-          "acceptAll": "সব গ্রহণ করুন",
-          "decline": "সব প্রত্যাখ্যান করুন"
+          "save": "स्वीकार करना",
+          "acceptAll": "सभी स्वीकृत",
+          "decline": "सभी अस्वीकार करें"
         }
       },
       "poweredBy": "Powered by Seqrite",
       "purposes": {
         "Analytics": {
-          "description": "<p>এই কুকিজ আমাদের ওয়েবসাইট ট্র্যাফিক, ব্যবহারকারীর আচরণ এবং ব্যস্ততার ধরণ বিশ্লেষণ করতে সাহায্য করে। সংগৃহীত অন্তর্দৃষ্টি আমাদের কন্টেন্ট অপ্টিমাইজ করতে, ব্যবহারযোগ্যতা বাড়াতে এবং সামগ্রিক সাইটের কর্মক্ষমতা উন্নত করতে সাহায্য করে।</p>"
+          "description": "<p>ये कुकीज़ हमें वेबसाइट ट्रैफ़िक, उपयोगकर्ता व्यवहार और जुड़ाव पैटर्न का विश्लेषण करने में मदद करती हैं। एकत्रित जानकारी हमें सामग्री को अनुकूलित करने, उपयोगिता बढ़ाने और समग्र साइट प्रदर्शन में सुधार करने में मदद करती है।</p>"
         },
         "Others": {
-          "description": "<p>এই বিভাগে এমন কুকিজ অন্তর্ভুক্ত রয়েছে যা পূর্বনির্ধারিত বিভাগগুলিতে খাপ খায় না তবে নির্দিষ্ট ওয়েবসাইটের কার্যকারিতা বা উন্নতির জন্য প্রয়োজনীয় হতে পারে।</p>"
+          "description": "<p>इस श्रेणी में वे कुकीज़ शामिल हैं जो पूर्वनिर्धारित श्रेणियों में फिट नहीं होती हैं, लेकिन कुछ वेबसाइट कार्यक्षमताओं या सुधारों के लिए आवश्यक हो सकती हैं।</p>"
         }
       },
       "service": {
@@ -332,11 +286,57 @@ window.klaroConfig = {
           "description": "Opt Out",
           "title": "Opt Out"
         },
-        "purpose": "উদ্দেশ্য"
+        "purpose": "उद्देश्य"
       },
       "purposeItem": {
-        "service": "সেবা",
-        "services": "সেবা"
+        "service": "सेवा",
+        "services": "सेवा"
+      }
+    },
+    "hi": {
+      "acceptAll": "सभी स्वीकृत",
+      "accept": "स्वीकार करना",
+      "decline": "सभी अस्वीकार करें",
+      "acceptSelected": "चयनित स्वीकार करें",
+      "ok": "सभी स्वीकृत",
+      "consentNotice": {
+        "description": "<p><strong>हम आपकी गोपनीयता का सम्मान करते हैं</strong></p><p> हम आवश्यक साइट कार्यक्षमता, साथ ही मार्केटिंग, वैयक्तिकरण और विश्लेषण को सक्षम करने के लिए कुकीज़ संग्रहीत करते हैं। &quot;सभी स्वीकार करें&quot; पर क्लिक करके, आप कुकीज़ के हमारे उपयोग के लिए सहमति देते हैं। आप अपनी सेटिंग कभी भी बदल सकते हैं। <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>कुकी नीति।</u></a></p>",
+        "learnMore": "अनुकूलित करें",
+        "changeDescription": " "
+      },
+      "save": "स्वीकार करना",
+      "consentModal": {
+        "title": "सहमति प्राथमिकताएँ अनुकूलित करें",
+        "description": "हम कुकीज़ का इस्तेमाल आपको पृष्ठों के बीच कुशलतापूर्वक नेविगेट करने, आपकी प्राथमिकताओं को संग्रहीत करने और वेबसाइट के आपके अनुभव को बेहतर बनाने में मदद करने के लिए करते हैं। हालाँकि, हम आपकी गोपनीयता के अधिकार को स्वीकार करते हैं और इसलिए आप कुछ प्रकार की कुकीज़ की अनुमति नहीं देने का विकल्प चुन सकते हैं। अधिक जानकारी प्राप्त करने और हमारी डिफ़ॉल्ट सेटिंग्स बदलने के लिए विभिन्न श्रेणी शीर्षकों पर क्लिक करें। कृपया ध्यान दें कि कुछ प्रकार की कुकीज़ को ब्लॉक करने से साइट के आपके अनुभव पर असर पड़ सकता है। ",
+        "buttons": {
+          "save": "स्वीकार करना",
+          "acceptAll": "सभी स्वीकृत",
+          "decline": "सभी अस्वीकार करें"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Analytics": {
+          "description": "<p>ये कुकीज़ हमें वेबसाइट ट्रैफ़िक, उपयोगकर्ता व्यवहार और जुड़ाव पैटर्न का विश्लेषण करने में मदद करती हैं। एकत्रित जानकारी हमें सामग्री को अनुकूलित करने, उपयोगिता बढ़ाने और समग्र साइट प्रदर्शन में सुधार करने में मदद करती है।</p>"
+        },
+        "Others": {
+          "description": "<p>इस श्रेणी में वे कुकीज़ शामिल हैं जो पूर्वनिर्धारित श्रेणियों में फिट नहीं होती हैं, लेकिन कुछ वेबसाइट कार्यक्षमताओं या सुधारों के लिए आवश्यक हो सकती हैं।</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "उद्देश्य"
+      },
+      "purposeItem": {
+        "service": "सेवा",
+        "services": "सेवा"
       }
     }
   },
@@ -351,11 +351,25 @@ window.klaroConfig = {
   "declarative": false,
   "groupByPurpose": true,
   "hideToggleAll": false,
-  "appName": "test Demo",
+  "appName": "Test for demo",
   "description": "We respects your privacy and is committed to protecting your personal data.",
-  "companyName": "test Demo",
+  "companyName": "Test for demo",
   "companyAddress": " ",
   "services": [
+    {
+      "name": "_ga_0000000001",
+      "title": "_ga_0000000001",
+      "purposes": [
+        "Others"
+      ],
+      "required": false,
+      "default": true,
+      "optOut": false,
+      "onlyOnce": true,
+      "cookies": [
+        "_ga_0000000001"
+      ]
+    },
     {
       "name": "_ga",
       "title": "_ga",
@@ -368,34 +382,6 @@ window.klaroConfig = {
       "onlyOnce": true,
       "cookies": [
         "_ga"
-      ]
-    },
-    {
-      "name": "_gat_gtag_UA_12345678_1",
-      "title": "_gat_gtag_UA_12345678_1",
-      "purposes": [
-        "Others"
-      ],
-      "required": false,
-      "default": true,
-      "optOut": false,
-      "onlyOnce": true,
-      "cookies": [
-        "_gat_gtag_UA_12345678_1"
-      ]
-    },
-    {
-      "name": "_gid",
-      "title": "_gid",
-      "purposes": [
-        "Analytics"
-      ],
-      "required": false,
-      "default": true,
-      "optOut": false,
-      "onlyOnce": true,
-      "cookies": [
-        "_gid"
       ]
     }
   ]
