a1089a2b8d9706e87c16979d7a78c6160e330492
Test-Net009 <nikhil.shelke@quickheal.com>
2025-09-04 13:54:47 +0530

    Update seqrite-config.js
    new consent form changes

--- seqrite-config.js
+++ seqrite-config.js
@@ -1,14 +1,14 @@
 
 let userLang = (navigator.language || (navigator).userLanguage || 'en').slice(0, 2);
 window.klaroConfig = {
-  "consentManagerId": "6899797c939bb223903abf65",
-  "uuid": "4c252af9-a633-4f52-a3b1-9c6b7749bdb3",
+  "consentManagerId": "68b94b3c8ab194795b70b94b",
+  "uuid": "b453119d-c502-4693-87fc-6d728199ace1",
   "domain": "https://dev-hs.qhtpl.com/cmapi",
   "version": 1,
   "elementID": "SEQRITE",
   lang: userLang,
   "storageMethod": "cookie",
-  "storageName": "https://cookieconsenttestdev.netlify.app/",
+  "storageName": "https://test-net009.github.io/Test-cookie-Consent/",
   "cookieExpiresAfterDays": 365,
   "default": false,
   "mustConsent": false,
@@ -16,7 +16,238 @@ window.klaroConfig = {
   "hideDeclineAll": false,
   "hideLearnMore": true,
   "disablePoweredBy": false,
-  "translations": {},
+  "translations": {
+    "english": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Analytics": {
+          "description": "<p>NA</p>"
+        },
+        "Others": {
+          "description": "<p>NA</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "purpose"
+      },
+      "purposeItem": {
+        "service": "service",
+        "services": "services"
+      }
+    },
+    "abri": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Analytics": {
+          "description": "<p>NA</p>"
+        },
+        "Others": {
+          "description": "<p>NA</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "purpose"
+      },
+      "purposeItem": {
+        "service": "service",
+        "services": "services"
+      }
+    },
+    "abrizz": {
+      "acceptAll": "Accept all",
+      "accept": "Accept",
+      "decline": "Reject All",
+      "acceptSelected": "Accept Selected",
+      "ok": "Accept All",
+      "consentNotice": {
+        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "learnMore": "Customize",
+        "changeDescription": " "
+      },
+      "save": "Accept",
+      "consentModal": {
+        "title": "Customize Consent Preferences",
+        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "buttons": {
+          "save": "Accept",
+          "acceptAll": "Accept all",
+          "decline": "Reject All"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Analytics": {
+          "description": "<p>NA</p>"
+        },
+        "Others": {
+          "description": "<p>NA</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "purpose"
+      },
+      "purposeItem": {
+        "service": "service",
+        "services": "services"
+      }
+    },
+    "marathi": {
+      "acceptAll": "सर्व स्वीकारा",
+      "accept": "स्वीकारा",
+      "decline": "सर्व नाकारा",
+      "acceptSelected": "निवडलेले स्वीकारा",
+      "ok": "सर्व स्वीकारा",
+      "consentNotice": {
+        "description": "<p><strong>आम्ही तुमच्या गोपनीयतेचा आदर करतो.</strong></p><p> आम्ही साइटची आवश्यक कार्यक्षमता तसेच मार्केटिंग, वैयक्तिकरण आणि विश्लेषण सक्षम करण्यासाठी कुकीज संग्रहित करतो. &quot;सर्व स्वीकारा&quot; वर क्लिक करून, तुम्ही आमच्या कुकीजच्या वापरास संमती देता. तुम्ही कधीही तुमच्या सेटिंग्ज बदलू शकता. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>कुकी धोरण.</u></a></p>",
+        "learnMore": "सानुकूलित करा",
+        "changeDescription": " "
+      },
+      "save": "स्वीकारा",
+      "consentModal": {
+        "title": "संमती प्राधान्ये कस्टमाइझ करा",
+        "description": "आम्ही कुकीजचा वापर तुम्हाला पृष्ठांमध्ये कार्यक्षमतेने नेव्हिगेट करण्यास, तुमच्या पसंती संग्रहित करण्यास आणि सामान्यतः वेबसाइटवरील तुमचा अनुभव सुधारण्यास मदत करण्यासाठी करतो. तथापि, आम्ही तुमचा गोपनीयतेचा अधिकार मान्य करतो आणि म्हणून तुम्ही काही प्रकारच्या कुकीजना परवानगी न देणे निवडू शकता. अधिक जाणून घेण्यासाठी आणि आमच्या डीफॉल्ट सेटिंग्ज बदलण्यासाठी वेगवेगळ्या श्रेणी शीर्षकांवर क्लिक करा. कृपया लक्षात ठेवा की विशिष्ट प्रकारच्या कुकीज अवरोधित केल्याने साइटवरील तुमच्या अनुभवावर परिणाम होऊ शकतो. ",
+        "buttons": {
+          "save": "स्वीकारा",
+          "acceptAll": "सर्व स्वीकारा",
+          "decline": "सर्व नाकारा"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Analytics": {
+          "description": "<p>ते</p>"
+        },
+        "Others": {
+          "description": "<p>ते</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "उद्देश"
+      },
+      "purposeItem": {
+        "service": "सेवा",
+        "services": "सेवा"
+      }
+    },
+    "hindi": {
+      "acceptAll": "सभी स्वीकृत",
+      "accept": "स्वीकार करना",
+      "decline": "सभी अस्वीकार करें",
+      "acceptSelected": "चयनित स्वीकार करें",
+      "ok": "सभी स्वीकृत",
+      "consentNotice": {
+        "description": "<p><strong>हम आपकी गोपनीयता का सम्मान करते हैं</strong></p><p> हम आवश्यक साइट कार्यक्षमता, साथ ही मार्केटिंग, वैयक्तिकरण और विश्लेषण को सक्षम करने के लिए कुकीज़ संग्रहीत करते हैं। &quot;सभी स्वीकार करें&quot; पर क्लिक करके, आप कुकीज़ के हमारे उपयोग के लिए सहमति देते हैं। आप अपनी सेटिंग कभी भी बदल सकते हैं। <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>कुकी नीति।</u></a></p>",
+        "learnMore": "अनुकूलित करें",
+        "changeDescription": " "
+      },
+      "save": "स्वीकार करना",
+      "consentModal": {
+        "title": "सहमति प्राथमिकताएँ अनुकूलित करें",
+        "description": "हम कुकीज़ का इस्तेमाल आपको पृष्ठों के बीच कुशलतापूर्वक नेविगेट करने, आपकी प्राथमिकताओं को संग्रहीत करने और वेबसाइट के आपके अनुभव को बेहतर बनाने में मदद करने के लिए करते हैं। हालाँकि, हम आपकी गोपनीयता के अधिकार को स्वीकार करते हैं और इसलिए आप कुछ प्रकार की कुकीज़ की अनुमति नहीं देने का विकल्प चुन सकते हैं। अधिक जानकारी प्राप्त करने और हमारी डिफ़ॉल्ट सेटिंग्स बदलने के लिए विभिन्न श्रेणी शीर्षकों पर क्लिक करें। कृपया ध्यान दें कि कुछ प्रकार की कुकीज़ को ब्लॉक करने से साइट के आपके अनुभव पर असर पड़ सकता है। ",
+        "buttons": {
+          "save": "स्वीकार करना",
+          "acceptAll": "सभी स्वीकृत",
+          "decline": "सभी अस्वीकार करें"
+        }
+      },
+      "poweredBy": "Powered by Seqrite",
+      "purposes": {
+        "Analytics": {
+          "description": "<p>वह</p>"
+        },
+        "Others": {
+          "description": "<p>वह</p>"
+        }
+      },
+      "service": {
+        "disableAll": {
+          "description": "DisableAll",
+          "title": "Disable"
+        },
+        "optOut": {
+          "description": "Opt Out",
+          "title": "Opt Out"
+        },
+        "purpose": "उद्देश्य"
+      },
+      "purposeItem": {
+        "service": "सेवा",
+        "services": "सेवा"
+      }
+    }
+  },
   "styling": {
     "theme": [
       "light",
@@ -28,9 +259,9 @@ window.klaroConfig = {
   "declarative": false,
   "groupByPurpose": true,
   "hideToggleAll": false,
-  "appName": "test2",
+  "appName": "devTest04092025_kk_01",
   "description": "We respects your privacy and is committed to protecting your personal data.",
-  "companyName": "test2",
+  "companyName": "devTest04092025_kk_01",
   "companyAddress": " ",
   "services": [
     {
@@ -40,7 +271,7 @@ window.klaroConfig = {
         "Others"
       ],
       "required": false,
-      "default": false,
+      "default": true,
       "optOut": false,
       "onlyOnce": true,
       "cookies": [
@@ -54,7 +285,7 @@ window.klaroConfig = {
         "Analytics"
       ],
       "required": false,
-      "default": false,
+      "default": true,
       "optOut": false,
       "onlyOnce": true,
       "cookies": [
