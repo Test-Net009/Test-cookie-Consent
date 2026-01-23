8371abce5f6b01c0fdc724e71f386e247a810388
Test-Net009 <nikhil.shelke@quickheal.com>
2026-01-23 16:23:53 +0530

    Add files via upload

--- seqrite-config.js
+++ seqrite-config.js
@@ -2,170 +2,22 @@
 let userLang = (navigator.language || (navigator).userLanguage || 'en');
 console.log("Browser Language Code: ", userLang);
 window.klaroConfig = {
-  "consentManagerId": "69731350f3387c313e9fe02a",
-  "uuid": "c202e52c-287f-478a-b490-e9ff2e922a0c",
+  "consentManagerId": "69734f57f3387c313e9fe034",
+  "uuid": "7e571d4c-0f9f-4d84-90f2-46b12e1edda6",
   "domain": "https://qa-op.pre-dataprivacy.com/cmapi",
   "version": 1,
   "elementID": "SEQRITE",
   lang: userLang,
   "storageMethod": "cookie",
-  "storageName": "www.seqrite.com",
+  "storageName": "https://cookieconsenttestqa.netlify.app/",
   "cookieExpiresAfterDays": 365,
   "default": false,
-  "mustConsent": true,
+  "mustConsent": false,
   "acceptAll": true,
   "hideDeclineAll": false,
   "hideLearnMore": false,
   "disablePoweredBy": false,
-  "translations": {
-    "english": {
-      "acceptAll": "Accept all",
-      "accept": "Accept",
-      "decline": "Reject All",
-      "acceptSelected": "Accept Selected",
-      "ok": "Accept All",
-      "consentNotice": {
-        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
-        "learnMore": "Customize",
-        "changeDescription": " "
-      },
-      "save": "Accept",
-      "consentModal": {
-        "title": "Customize Consent Preferences",
-        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
-        "buttons": {
-          "save": "Accept",
-          "acceptAll": "Accept all",
-          "decline": "Reject All"
-        }
-      },
-      "poweredBy": "Powered by Seqrite",
-      "purposes": {
-        "Others": {
-          "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p>"
-        },
-        "Analytics": {
-          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance. </p>"
-        },
-        "Marketing": {
-          "description": "<p>These cookies are used by our advertising partners to deliver personalized ads and measure the effectiveness of marketing campaigns. They help provide relevant promotions based on user interests and browsing behavior. </p>"
-        }
-      },
-      "service": {
-        "disableAll": {
-          "description": "DisableAll",
-          "title": "Disable"
-        },
-        "optOut": {
-          "description": "Opt Out",
-          "title": "Opt Out"
-        },
-        "purpose": "purpose"
-      },
-      "purposeItem": {
-        "service": "service",
-        "services": "services"
-      }
-    },
-    "en": {
-      "acceptAll": "Accept all",
-      "accept": "Accept",
-      "decline": "Reject All",
-      "acceptSelected": "Accept Selected",
-      "ok": "Accept All",
-      "consentNotice": {
-        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
-        "learnMore": "Customize",
-        "changeDescription": " "
-      },
-      "save": "Accept",
-      "consentModal": {
-        "title": "Customize Consent Preferences",
-        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
-        "buttons": {
-          "save": "Accept",
-          "acceptAll": "Accept all",
-          "decline": "Reject All"
-        }
-      },
-      "poweredBy": "Powered by Seqrite",
-      "purposes": {
-        "Others": {
-          "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p>"
-        },
-        "Analytics": {
-          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance. </p>"
-        },
-        "Marketing": {
-          "description": "<p>These cookies are used by our advertising partners to deliver personalized ads and measure the effectiveness of marketing campaigns. They help provide relevant promotions based on user interests and browsing behavior. </p>"
-        }
-      },
-      "service": {
-        "disableAll": {
-          "description": "DisableAll",
-          "title": "Disable"
-        },
-        "optOut": {
-          "description": "Opt Out",
-          "title": "Opt Out"
-        },
-        "purpose": "purpose"
-      },
-      "purposeItem": {
-        "service": "service",
-        "services": "services"
-      }
-    },
-    "zz": {
-      "acceptAll": "Accept all",
-      "accept": "Accept",
-      "decline": "Reject All",
-      "acceptSelected": "Accept Selected",
-      "ok": "Accept All",
-      "consentNotice": {
-        "description": "<p><strong>We respect your privacy</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
-        "learnMore": "Customize",
-        "changeDescription": " "
-      },
-      "save": "Accept",
-      "consentModal": {
-        "title": "Customize Consent Preferences",
-        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
-        "buttons": {
-          "save": "Accept",
-          "acceptAll": "Accept all",
-          "decline": "Reject All"
-        }
-      },
-      "poweredBy": "Powered by Seqrite",
-      "purposes": {
-        "Others": {
-          "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p>"
-        },
-        "Analytics": {
-          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance. </p>"
-        },
-        "Marketing": {
-          "description": "<p>These cookies are used by our advertising partners to deliver personalized ads and measure the effectiveness of marketing campaigns. They help provide relevant promotions based on user interests and browsing behavior. </p>"
-        }
-      },
-      "service": {
-        "disableAll": {
-          "description": "DisableAll",
-          "title": "Disable"
-        },
-        "optOut": {
-          "description": "Opt Out",
-          "title": "Opt Out"
-        },
-        "purpose": "purpose"
-      },
-      "purposeItem": {
-        "service": "service",
-        "services": "services"
-      }
-    }
-  },
+  "translations": {},
   "styling": {
     "theme": [
       "light",
@@ -177,23 +29,23 @@ window.klaroConfig = {
   "declarative": false,
   "groupByPurpose": true,
   "hideToggleAll": false,
-  "appName": "Test_7",
+  "appName": "2.3.4 Manish Cookies Regression",
   "description": "We respects your privacy and is committed to protecting your personal data.",
-  "companyName": "Test_7",
+  "companyName": "2.3.4 Manish Cookies Regression",
   "companyAddress": " ",
   "services": [
     {
-      "name": "_gat_UA-104093208-1",
-      "title": "_gat_UA-104093208-1",
+      "name": "_ga_0000000001",
+      "title": "_ga_0000000001",
       "purposes": [
         "Others"
       ],
       "required": false,
-      "default": false,
+      "default": true,
       "optOut": false,
       "onlyOnce": true,
       "cookies": [
-        "_gat_UA-104093208-1"
+        "_ga_0000000001"
       ]
     },
     {
@@ -203,53 +55,11 @@ window.klaroConfig = {
         "Analytics"
       ],
       "required": false,
-      "default": false,
+      "default": true,
       "optOut": false,
       "onlyOnce": true,
       "cookies": [
         "_ga"
-      ]
-    },
-    {
-      "name": "_gcl_au",
-      "title": "_gcl_au",
-      "purposes": [
-        "Marketing"
-      ],
-      "required": false,
-      "default": false,
-      "optOut": false,
-      "onlyOnce": true,
-      "cookies": [
-        "_gcl_au"
-      ]
-    },
-    {
-      "name": "_ga_HZ8CDYG4V1",
-      "title": "_ga_HZ8CDYG4V1",
-      "purposes": [
-        "Others"
-      ],
-      "required": false,
-      "default": false,
-      "optOut": false,
-      "onlyOnce": true,
-      "cookies": [
-        "_ga_HZ8CDYG4V1"
-      ]
-    },
-    {
-      "name": "_gid",
-      "title": "_gid",
-      "purposes": [
-        "Analytics"
-      ],
-      "required": false,
-      "default": false,
-      "optOut": false,
-      "onlyOnce": true,
-      "cookies": [
-        "_gid"
       ]
     }
   ]
