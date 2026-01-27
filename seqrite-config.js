71c5eaf1f21e8ad9495eb3813a431342008508a0
Test-Net009 <nikhil.shelke@quickheal.com>
2026-01-27 17:36:30 +0530

    Testing

--- seqrite-config.js
+++ seqrite-config.js
@@ -3,7 +3,7 @@ let userLang = (navigator.language || (navigator).userLanguage || 'en');
 console.log("Browser Language Code: ", userLang);
 window.klaroConfig = {
   "consentManagerId": "6974ccc61a2bc74b5dd8c937",
-  "uuid": "9f068444-306d-4acb-b1d2-dd4e948132de",
+  "uuid": "47d5c118-460b-4728-8ce7-16f127650918",
   "domain": "https://preprod.pre-dataprivacy.com/cmapi",
   "version": 1,
   "elementID": "SEQRITE",
@@ -22,17 +22,17 @@ window.klaroConfig = {
       "acceptAll": "Accept all",
       "accept": "Accept",
       "decline": "Reject All",
-      "acceptSelected": "Accept Selected",
-      "ok": "Accept All",
+      "acceptSelected": "Reject Non-Essen",
+      "ok": "Accept all",
       "consentNotice": {
-        "description": "<p><strong>We respect your privacy - 27th Jan 2027</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "description": "<p><strong>We respect your privacy - 27th Jan 2028</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
         "learnMore": "Customize",
         "changeDescription": " "
       },
       "save": "Accept",
       "consentModal": {
-        "title": "<p>Customize Consent Preferences - 26th Jan 2027</p>",
-        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "title": "<p>Customize Consent Preferences - 26th Jan 2028</p>",
+        "description": "<p><strong>We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website. However, we acknowledge your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site.</strong></p>",
         "buttons": {
           "save": "Accept",
           "acceptAll": "Accept all",
@@ -41,11 +41,11 @@ window.klaroConfig = {
       },
       "poweredBy": "Powered by Seqrite",
       "purposes": {
-        "Analytics": {
-          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance.</p>"
-        },
         "Others": {
           "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p>"
+        },
+        "Functional": {
+          "description": "<p>We use these cookies to enhance website functionality and improve user experience. Information can include login data, region, language, and enhanced content.</p>"
         }
       },
       "service": {
@@ -68,17 +68,17 @@ window.klaroConfig = {
       "acceptAll": "Accept all",
       "accept": "Accept",
       "decline": "Reject All",
-      "acceptSelected": "Accept Selected",
-      "ok": "Accept All",
+      "acceptSelected": "Reject Non-Essen",
+      "ok": "Accept all",
       "consentNotice": {
-        "description": "<p><strong>We respect your privacy - 27th Jan 2027</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "description": "<p><strong>We respect your privacy - 27th Jan 2028</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
         "learnMore": "Customize",
         "changeDescription": " "
       },
       "save": "Accept",
       "consentModal": {
-        "title": "<p>Customize Consent Preferences - 26th Jan 2027</p>",
-        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "title": "<p>Customize Consent Preferences - 26th Jan 2028</p>",
+        "description": "<p><strong>We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website. However, we acknowledge your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site.</strong></p>",
         "buttons": {
           "save": "Accept",
           "acceptAll": "Accept all",
@@ -87,11 +87,11 @@ window.klaroConfig = {
       },
       "poweredBy": "Powered by Seqrite",
       "purposes": {
-        "Analytics": {
-          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance.</p>"
-        },
         "Others": {
           "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p>"
+        },
+        "Functional": {
+          "description": "<p>We use these cookies to enhance website functionality and improve user experience. Information can include login data, region, language, and enhanced content.</p>"
         }
       },
       "service": {
@@ -114,17 +114,17 @@ window.klaroConfig = {
       "acceptAll": "Accept all",
       "accept": "Accept",
       "decline": "Reject All",
-      "acceptSelected": "Accept Selected",
-      "ok": "Accept All",
+      "acceptSelected": "Reject Non-Essen",
+      "ok": "Accept all",
       "consentNotice": {
-        "description": "<p><strong>We respect your privacy - 27th Jan 2027</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
+        "description": "<p><strong>We respect your privacy - 27th Jan 2028</strong></p><p>We store cookies to enable essential site functionality, as well as marketing, personalization and analytics. By clicking “Accept All”, you consent to our use of cookies. You can change your settings at any time. <a href=\"https://www.google.com/\" title=\"https://www.google.com/\" target=\"_blank\"><u>Cookie Policy.</u></a></p>",
         "learnMore": "Customize",
         "changeDescription": " "
       },
       "save": "Accept",
       "consentModal": {
-        "title": "<p>Customize Consent Preferences - 26th Jan 2027</p>",
-        "description": "We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website.  However, we acknowledge  your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site. ",
+        "title": "<p>Customize Consent Preferences - 26th Jan 2028</p>",
+        "description": "<p><strong>We use cookies to help you navigate between pages efficiently, storing your preferences, and generally improving your experience of a website. However, we acknowledge your right to privacy and hence you can choose not to allow some types of cookies. Click on the different category headings to find out more and change our default settings. Please note that blocking certain types of cookies might affect your experience of the site.</strong></p>",
         "buttons": {
           "save": "Accept",
           "acceptAll": "Accept all",
@@ -133,11 +133,11 @@ window.klaroConfig = {
       },
       "poweredBy": "Powered by Seqrite",
       "purposes": {
-        "Analytics": {
-          "description": "<p>These cookies help us analyze website traffic, user behavior, and engagement patterns. The insights gathered allow us to optimize content, enhance usability, and improve overall site performance.</p>"
-        },
         "Others": {
           "description": "<p>This category includes cookies that do not fit into the predefined categories but may be necessary for certain website functionalities or improvements.</p>"
+        },
+        "Functional": {
+          "description": "<p>We use these cookies to enhance website functionality and improve user experience. Information can include login data, region, language, and enhanced content.</p>"
         }
       },
       "service": {
@@ -168,9 +168,9 @@ window.klaroConfig = {
   "declarative": false,
   "groupByPurpose": true,
   "hideToggleAll": false,
-  "appName": "Test for demo",
+  "appName": "Test for demo1",
   "description": "We respects your privacy and is committed to protecting your personal data.",
-  "companyName": "Test for demo",
+  "companyName": "Test for demo1",
   "companyAddress": " ",
   "services": [
     {
@@ -191,7 +191,7 @@ window.klaroConfig = {
       "name": "_ga",
       "title": "_ga",
       "purposes": [
-        "Analytics"
+        "Functional"
       ],
       "required": false,
       "default": false,
